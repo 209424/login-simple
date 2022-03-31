15545e613eeeabc070fab5a42c4ea55077732934
209424 <209424@edu.p.lodz.pl>
2022-03-31 17:16:46 +0200

    Register button working (?)

--- app/Client/src/App.js
+++ app/Client/src/App.js
@@ -1,16 +1,36 @@
 import React, {useState} from 'react';
+import Axios from 'axios';
 import './App.css';
 
 function App() {
+
+	const [usernameReg, setUsernameReg] = useState('');
+	const [passwordReg, setPasswordReg] = useState('');
+
+	const register = _ => {
+		Axios.post('http://localhost:3001/register', {
+			username: usernameReg,
+			password: passwordReg
+		}).then(response => {
+			console.log(response);
+		}).catch(error => {
+			console.log(error);
+		});
+	}
+
 	return (
 		<div className='App'>
 			<div className='registration'>
 				<h1>Registration</h1>
 				<label>Username</label>
-				<input type='text'/>
+				<input type='text' onChange={event => {
+					setUsernameReg(event.target.value);
+				}}/>
 				<label>Password</label>
-				<input type='password'/>
-				<button>Register</button>
+				<input type='password' onChange={event => {
+					setPasswordReg(event.target.value);
+				}}/>
+				<button onClick={register}>Register</button>
 			</div>
 
 			<div className='login'>
--- app/Server/index.js
+++ app/Server/index.js
@@ -5,12 +5,28 @@ const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const db = mysql.createConnection({
 	user: "root",
 	host: "localhost",
 	password: "password",
 	database: "LoginSystem"
+});
+
+app.post('/register', (req, res) => {
+	const username = req.body.username;
+	const password = req.body.password;
+
+	db.query('INSERT INTO users (username, password) VALUES (?, ?)',
+		[username, password],
+		(err, result) => {
+		if (err) {
+			res.status(500).send(err);
+		} else {
+			res.status(200).send(result);
+		}
+	});
 });
 
 app.listen(3001, _ => {
